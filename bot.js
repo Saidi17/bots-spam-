a544316fcc2d63c7c13986a6b2ee7cc496c217c2
Saidi17 <73559247+Saidi17@users.noreply.github.com>
2020-10-29 12:09:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("حط الاي دي حق الروم اللي يحصل بيها الاسبام هنا")
+let channel =     client.channels.get("770737329337532470")
 setInterval(function() {
-channel.send(`حط الكلام اللي تبغاه هنا`);
+channel.send(`Saidi Bell  Saidi Bell`);
 }, 30)
 })
 
